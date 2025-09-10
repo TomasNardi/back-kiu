e5a8aa6d20d90e00bfa77fd4afe6522ed0afa00c
Tomas Nardi <tomas.nardi@hotmail.com>
2025-09-10 11:08:39 -0300

    feat(flights): configure URLs and endpoint for flight search

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+
+# Import 'include' to recognize URLs of our kiu_vuelos app
+from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include('kiu_vuelos.urls')),
 ]
---
+++ backend/kiu_vuelos/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Endpoint for searching flights
+    path('journeys/search', views.search_flights, name='search_flight'),
+]
