66a8e36a30b63a00cf3c7e329c33c75fb54ff03e
Tomas Nardi <tomas.nardi@hotmail.com>
2025-09-11 09:40:49 -0300

    deploy 3

--- backend/kiu_flights/apps.py
+++ backend/kiu_flights/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class KiuFlightsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'backend.kiu_flights'
+    name = 'kiu_flights'
